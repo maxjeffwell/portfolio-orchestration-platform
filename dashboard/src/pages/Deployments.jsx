f27f4de3686fd6787e89f69d18d3dd5cf72457eb
maxjeffwell <maxjeffwell@gmail.com>
2025-12-16 10:03:56 -0500

    feat: migrate educationelly-graphql to subdomain routing
    - Create subdomain ingress for educationelly-graphql-k8s.el-jefe.me
    - Remove /educationelly-graphql path from portfolio-apps-ingress
    - Update dashboard to show new subdomain URL

--- dashboard/src/pages/Deployments.jsx
+++ dashboard/src/pages/Deployments.jsx
@@ -34,7 +34,7 @@ const APP_URLS = {
   'firebook': 'https://pop-portfolio.el-jefe.me/firebook/',
   'intervalai-client': 'https://pop-portfolio.el-jefe.me/intervalai/',
   'educationelly-client': 'https://educationelly-k8s.el-jefe.me/',
-  'educationelly-graphql-client': 'https://pop-portfolio.el-jefe.me/educationelly-graphql/',
+  'educationelly-graphql-client': 'https://educationelly-graphql-k8s.el-jefe.me/',
   'code-talk-client': 'https://code-talk-k8s.el-jefe.me/',
   'tenantflow-frontend': 'https://tenantflow.el-jefe.me/',
   'devops-portfolio-dashboard': 'https://podrick.el-jefe.me/',
